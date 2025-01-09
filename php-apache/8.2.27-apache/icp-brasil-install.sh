5b18629b4ef5f308bd990a2c93bc240aa5d25ac6
iderlei <iderlei@atua.com.br>
2025-01-09 09:31:16 -0300

    ajustando download dos certificados icp-brasil

--- php-apache/8.2.27-apache/icp-brasil-install.sh
+++ php-apache/8.2.27-apache/icp-brasil-install.sh
@@ -5,7 +5,7 @@ ARQUIVO="ACcompactado.zip"
 cd /usr/local/share/ca-certificates
 
 rm -f $ARQUIVO
-if wget -O $ARQUIVO https://acraiz.icpbrasil.gov.br/credenciadas/CertificadosAC-ICP-Brasil/$ARQUIVO; then
+if wget --no-check-certificate -O $ARQUIVO https://acraiz.icpbrasil.gov.br/credenciadas/CertificadosAC-ICP-Brasil/$ARQUIVO; then
   unzip -o $ARQUIVO
   update-ca-certificates
 fi
